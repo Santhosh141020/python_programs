fc21261d6fea873b70224b289f9d97fa109b32e2
Shubham Kumar shaw <31533697+Shubham2443@users.noreply.github.com>
2021-01-22 17:45:11 +0530

    Update hello.py
    Testing for merge conflictt

--- hello.py
+++ hello.py
@@ -2,3 +2,11 @@ print("Hello world")
 name =input("Enter your name\n")
 print("Hello"+ name)
 
+
+print("Hello world")
+name =input("Enter your name\n")
+print("Hello"+ name)
+
+print("Hello world")
+name =input("Enter your name\n")
+print("Hello"+ name)
